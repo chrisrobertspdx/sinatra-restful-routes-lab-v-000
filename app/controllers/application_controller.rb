e6160ffea6369d572ac7f7fbbf8bb90031790760
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-22 17:54:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,42 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
+  get '/recipes/new' do
+
+    erb :new
+  end
+  post '/recipes' do
+    #add
+    Recipe.create(params)
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    #get post by id
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    # @post = get post by params[:id]
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    #get the post by id then update
+    @recipe = Recipes.find(params[:id])
+    @recipe.update(name: params[:name])
+    @recipe.update(ingredients: params[:ingredients])
+    @recipe.update(cook_time: params[:cook_time])
+    erb :show
+  end
+
+  delete '/recipes/:id/delete' do
+    #get the post by id then delete
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    erb :delete
+  end
 end
